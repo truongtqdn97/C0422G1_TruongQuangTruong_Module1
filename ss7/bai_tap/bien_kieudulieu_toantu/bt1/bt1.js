1b86ef943d1ed6576ee292045ca5023392f3a0c9
truongtqdn97 <truong.dn97@gmail.com>
2022-05-10 14:19:15 +0700

    nop bai

---
+++ ss7/bai_tap/bien_kieudulieu_toantu/bt1/bt1.js
@@ -0,0 +1,9 @@
+let phy = parseInt(prompt("Enter Physics point: "));
+let che = parseInt(prompt("Enter Chemistry point: "));
+let bio = parseInt(prompt("Enter Biology point: "));
+
+let avg = (phy+che+bio)/3;
+let total = phy+che+bio;
+
+document.write("the average of 3 subjects is: "+avg);
+document.write("the total of 3 subjects is: "+total);
---
+++ ss7/bai_tap/bien_kieudulieu_toantu/bt2/bt2.js
@@ -0,0 +1,3 @@
+let c = parseInt(prompt("nhap do C: "));
+let f = ((c/5)*9)+32;
+document.write("do F la: "+f);
