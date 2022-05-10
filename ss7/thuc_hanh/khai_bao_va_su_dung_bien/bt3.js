365321b816dff681e780054dc322f04116187446
truongtqdn97 <truong.dn97@gmail.com>
2022-05-10 10:46:01 +0700

    nop bai khai bao va su dung bien

---
+++ ss7/thuc_hanh/khai_bao_va_su_dung_bien/bt3.js
@@ -0,0 +1,7 @@
+let a = parseInt(prompt('nhập vào số a'));
+let b = parseInt(prompt('nhập vào số b'));
+if (a%b==0)
+    document.write("a la boi so cua b");
+else
+    document.write("a ko phai la boi so cua b")
+// document.write("tong cua a va b la: " + (a+b));
