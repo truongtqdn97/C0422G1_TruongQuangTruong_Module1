aad1bc070b009c3d0209531e0469cca483b86cf4
truongtqdn97 <truong.dn97@gmail.com>
2022-05-11 11:19:32 +0700

    nam nhuan cach 2

--- ss8/thuc_hanh/nam_nhuan/nam_nhuan.js
+++ ss8/thuc_hanh/nam_nhuan/nam_nhuan.js
@@ -1,14 +1,34 @@
     let year = parseInt(prompt("Enter a year"));
+    // cach 1:
+    // if (year % 4 == 0) {
+    //     if (year % 100 == 0) {
+    //         if (year % 400 == 0) {
+    //             alert(year + " is a leap year");
+    //         } else {
+    //             alert(year + " is NOT a leap year");
+    //         }
+    //     } else {
+    //         alert(year + " is a leap year");
+    //     }
+    // } else {
+    //     alert(year + " is NOT a leap year");
+    // }
+
+    // cach 2:
+    // let year = parseInt(prompt("Enter a year"));
+    let isLeapYear = false;
+
     if (year % 4 == 0) {
         if (year % 100 == 0) {
             if (year % 400 == 0) {
-                alert(year + " is a leap year");
-            } else {
-                alert(year + " is NOT a leap year");
+                isLeapYear = true;
             }
         } else {
-            alert(year + " is a leap year");
+            isLeapYear = true;
         }
+    }
+    if (isLeapYear) {
+        alert(year + " is a leap year");
     } else {
         alert(year + " is NOT a leap year");
     }
